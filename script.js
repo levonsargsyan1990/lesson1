b2821d6d193045f37503ad2c6bdff22918beac1c
Artsrun <arts.karapetyan@gmail.com>
2019-05-11 15:43:31 +0400

    first step done

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log('Welcome to Lesson 1 of JavaScript! Feel free to remove this line.')
+
+var a = 0;
+
+for (var i = 0; i < 5; i++) {
+   
+    for(var j = 0; j<5;j++){
+        process.stdout.write('  ' + a) ;}
+
+
+        process.stdout.write('\n');
+
+    
+    
+    
+}
