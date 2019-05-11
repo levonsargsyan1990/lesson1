dd4b2a2dd3a9f32e5a547c2a13eaa4dfed12c35c
Vars <hovhannisyanvarsenik@gmail.com>
2019-05-11 15:34:31 +0400

    Step1 implemented

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Welcome to Lesson 1 of JavaScript! Feel free to remove this line.')
+console.log(
+  "Welcome to Lesson 1 of JavaScript! Feel free to remove this line."
+);
+
+for (var i = 0; i <= 4; i++) {
+  console.log("\n");
+  for (var j = 0; j <= 4; j++) {
+    var x = 0;
+    process.stdout.write(" " + x);
+  }
+}
