aef61c024670254883545d85df39b464201a4c21
Zareh <zsargsyan89@gmail.com>
2019-05-11 15:43:45 +0400

    First task done

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('Welcome to Lesson 1 of JavaScript! Feel free to remove this line.')
+var size = 5,
+    i, j;
+
+for (i = 0; i < size; i++) {
+    let row = '';
+    for (j = 0; j < size; j++) {
+        row += '0   ';
+    }
+    console.log(row + "\n");
+}
