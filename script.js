fb75fcbc341703323e2c68216e34032ba43c5f95
Zareh <zsargsyan89@gmail.com>
2019-05-12 18:41:47 +0400

    task 4 done

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-console.log('Welcome to Lesson 1 of JavaScript! Feel free to remove this line.')
-var size = 5,
-    i, j;
+// var size = 5,
+//     i, j;
 
-for (i = 0; i < size; i++) {
-    let row = '';
-    for (j = 0; j < size; j++) {
-        row += '0   ';
-    }
-    console.log(row + "\n");
+// for (i = 0; i < size; i++) {
+//     let row = '';
+//     for (j = 0; j < size; j++) {
+//         row += '0   ';
+//     }
+//     console.log(row + "\n");
+// }
+
+
+function matrix(x,y,interval){
+    return setTimeout(() => {
+        for (i = 0; i < y; i++) {
+            let row = '';
+            for (j = 0; j < x; j++) {
+                row += '0   ';
+            }
+            console.log(row + "\n")
+          }
+    }, interval);
 }
+
+matrix(4,4,3000)
+
+
