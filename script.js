2d396b078a15cdfef12da8c14bda844b8ba0ba32
Levon <levonsargsyan1990@gmail.com>
2019-05-14 13:58:39 +0400

    Moving to TypeScript

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Welcome to Lesson 1 of JavaScript! Feel free to remove this line.')
+console.log(
+  "Welcome to Lesson 1 of JavaScript! Feel free to remove this line."
+);
+console.log(
+  "Welcome to Lesson 1 of TypeScript! Feel free to remove this line."
+);
