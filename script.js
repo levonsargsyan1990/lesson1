e5be2e6730ed56defd0df616110057a695e5ac03
Levon <levonsargsyan1990@gmail.com>
2019-05-14 13:54:06 +0400

    more logging added

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ console.log(
 console.log(
   "Welcome to Lesson 1 of JavaScript! Feel free to remove this line."
 );
+console.log(
+  "Welcome to Lesson 1 of JavaScript! Feel free to remove this line."
+);
