00897f6e7b474d514c56168d2190647b76fc1bea
Artsrun <arts.karapetyan@gmail.com>
2019-05-15 14:28:11 +0400

    step 1,2,3,4 bonus 1

--- script.js
+++ script.js
@@ -1,20 +1,71 @@
-console.log('Welcome to Lesson 1 of JavaScript! Feel free to remove this line.')
 
-var a = 0;
+        var counter = 0;
+        function run(width, length, time, symbol1 , symbol2) {
+            counter++;
+            let keys = [];
+            for (var i = 0; i < length; i++) {
+                for (var j = 0; j < width; j++) {
+                    keys.push("" + i + j);
+                }
+            }
 
-for (var i = 0; i < 5; i++) {
-   
-    for(var j = 0; j<5;j++){
-        if(i==3 & j==3){a=1}
-        else {
-            a=0;
+            shuffle(keys);
+            var currentKeys = [];
+
+            while(keys.length) {
+                sleep(time);
+                printCube(currentKeys, width, length, symbol1 , symbol2);
+                currentKeys.push(keys.pop());
+            }
+
+            printCube(currentKeys, width, length, symbol1 , symbol2);
+            console.log("Congratulations! The process is complete: " + counter);
         }
-        process.stdout.write('  ' + a) ;}
+
+        function sleep(delay) {
+            var start = new Date().getTime();
+            while (new Date().getTime() < start + delay);
+        }
+
+        function printCube(indexes = [], width, length, symbol1 , symbol2) {
+            let line = "";
+            for (var i = 0; i < length; i++) {
+                for (var j = 0; j < width; j++) {
+                    if (indexes.indexOf("" + i + j) !== -1) {
+                        line += symbol2;
+                    } else {
+                        line += symbol1;
+                    }
+                }
+                line += "\n";
+            }
+            console.log(line);
+        }
+
+        function shuffle(a) {
+            var j, x, i;
+            for (i = a.length - 1; i > 0; i--) {
+                j = Math.floor(Math.random() * (i + 1));
+                x = a[i];
+                a[i] = a[j];
+                a[j] = x;
+            }
+            // return a;
+        }
         
+        function start() {
+            var width = parseInt(document.getElementById('width').value);
+            var height = parseInt(document.getElementById('height').value);
+            var time = parseInt(document.getElementById('time').value);
+            var symbol1 = document.getElementById('symbol1').value;
+            var symbol2 = document.getElementById('symbol2').value;
 
-        process.stdout.write('\n');
+            width = width ? width : 5;
+            height = height ? height : 5;
+            time = time ? time : 1000;
+            symbol1 = symbol1 ? symbol1 : "0";
+            symbol2 = symbol2 ? symbol2 : "1";
 
-    
-    
-    
-}
+            run(width, height, time, symbol1 , symbol2);
+        }
+ 
